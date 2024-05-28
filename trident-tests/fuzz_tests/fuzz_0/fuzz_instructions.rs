a1bcd51cec1861d457bdc039d773fde410cd6731
Ikrk <ikrk@centrum.cz>
2024-05-28 14:12:47 +0200

    Fuzz tests

---
+++ trident-tests/fuzz_tests/fuzz_0/fuzz_instructions.rs
@@ -0,0 +1,177 @@
+pub mod unauthorized_access_2_fuzz_instructions {
+    use crate::accounts_snapshots::*;
+    use trident_client::{fuzzing::*, solana_sdk::native_token::LAMPORTS_PER_SOL};
+    use unauthorized_access_2::ESCROW_SEED;
+    #[derive(Arbitrary, DisplayIx, FuzzTestExecutor, FuzzDeserialize)]
+    pub enum FuzzInstruction {
+        Initialize(Initialize),
+        Withdraw(Withdraw),
+    }
+    #[derive(Arbitrary, Debug)]
+    pub struct Initialize {
+        pub accounts: InitializeAccounts,
+        pub data: InitializeData,
+    }
+    #[derive(Arbitrary, Debug)]
+    pub struct InitializeAccounts {
+        #[arbitrary(with = |u: &mut arbitrary::Unstructured| u.int_in_range(1..=3))]
+        pub author: AccountId,
+        #[arbitrary(with = |u: &mut arbitrary::Unstructured| u.int_in_range(1..=3))]
+        pub escrow: AccountId,
+        pub system_program: AccountId,
+    }
+    #[derive(Arbitrary, Debug)]
+    pub struct InitializeData {
+        #[arbitrary(with = |u: &mut arbitrary::Unstructured| u.int_in_range(1..=3))]
+        pub receiver: AccountId,
+        pub amount: u64,
+    }
+    #[derive(Arbitrary, Debug)]
+    pub struct Withdraw {
+        pub accounts: WithdrawAccounts,
+        pub data: WithdrawData,
+    }
+    #[derive(Arbitrary, Debug)]
+    pub struct WithdrawAccounts {
+        #[arbitrary(with = |u: &mut arbitrary::Unstructured| u.int_in_range(1..=3))]
+        pub receiver: AccountId,
+        #[arbitrary(with = |u: &mut arbitrary::Unstructured| u.int_in_range(1..=3))]
+        pub escrow: AccountId,
+        pub system_program: AccountId,
+    }
+    #[derive(Arbitrary, Debug)]
+    pub struct WithdrawData {}
+
+    impl<'info> IxOps<'info> for Initialize {
+        type IxData = unauthorized_access_2::instruction::Initialize;
+        type IxAccounts = FuzzAccounts;
+        type IxSnapshot = InitializeSnapshot<'info>;
+        fn get_data(
+            &self,
+            client: &mut impl FuzzClient,
+            fuzz_accounts: &mut FuzzAccounts,
+        ) -> Result<Self::IxData, FuzzingError> {
+            let receiver = fuzz_accounts.receiver.get_or_create_account(
+                self.data.receiver,
+                client,
+                10 * LAMPORTS_PER_SOL,
+            );
+
+            let data = unauthorized_access_2::instruction::Initialize {
+                receiver: receiver.pubkey(),
+                amount: self.data.amount,
+            };
+            Ok(data)
+        }
+        fn get_accounts(
+            &self,
+            client: &mut impl FuzzClient,
+            fuzz_accounts: &mut FuzzAccounts,
+        ) -> Result<(Vec<Keypair>, Vec<AccountMeta>), FuzzingError> {
+            let author = fuzz_accounts.author.get_or_create_account(
+                self.accounts.author,
+                client,
+                10 * LAMPORTS_PER_SOL,
+            );
+
+            let receiver = fuzz_accounts.receiver.get_or_create_account(
+                self.data.receiver,
+                client,
+                10 * LAMPORTS_PER_SOL,
+            );
+
+            let escrow = fuzz_accounts.escrow.get_or_create_account(
+                self.accounts.escrow,
+                &[
+                    author.pubkey().as_ref(),
+                    receiver.pubkey().as_ref(),
+                    ESCROW_SEED.as_ref(),
+                ],
+                &unauthorized_access_2::ID,
+            ).unwrap();
+
+            let signers = vec![author.clone()];
+            let acc_meta = unauthorized_access_2::accounts::Initialize {
+                author: author.pubkey(),
+                escrow: escrow.pubkey(),
+                system_program: SYSTEM_PROGRAM_ID,
+            }
+            .to_account_metas(None);
+            Ok((signers, acc_meta))
+        }
+    }
+    impl<'info> IxOps<'info> for Withdraw {
+        type IxData = unauthorized_access_2::instruction::Withdraw;
+        type IxAccounts = FuzzAccounts;
+        type IxSnapshot = WithdrawSnapshot<'info>;
+        fn get_data(
+            &self,
+            _client: &mut impl FuzzClient,
+            _fuzz_accounts: &mut FuzzAccounts,
+        ) -> Result<Self::IxData, FuzzingError> {
+            let data = unauthorized_access_2::instruction::Withdraw {};
+            Ok(data)
+        }
+        fn get_accounts(
+            &self,
+            client: &mut impl FuzzClient,
+            fuzz_accounts: &mut FuzzAccounts,
+        ) -> Result<(Vec<Keypair>, Vec<AccountMeta>), FuzzingError> {
+            let receiver = fuzz_accounts.receiver.get_or_create_account(
+                self.accounts.receiver,
+                client,
+                10 * LAMPORTS_PER_SOL,
+            );
+
+            let author = fuzz_accounts.author.get_or_create_account(
+                self.accounts.receiver,
+                client,
+                10 * LAMPORTS_PER_SOL,
+            );
+
+            let escrow = fuzz_accounts.escrow.get_or_create_account(
+                self.accounts.escrow,
+                &[
+                    author.pubkey().as_ref(),
+                    receiver.pubkey().as_ref(),
+                    ESCROW_SEED.as_ref(),
+                ],
+                &unauthorized_access_2::ID,
+            ).unwrap();
+            let signers = vec![receiver.clone()];
+            let acc_meta = unauthorized_access_2::accounts::Withdraw {
+                receiver: receiver.pubkey(),
+                escrow: escrow.pubkey(),
+                system_program: SYSTEM_PROGRAM_ID,
+            }
+            .to_account_metas(None);
+            Ok((signers, acc_meta))
+        }
+
+        fn check(
+            &self,
+            pre_ix: Self::IxSnapshot,
+            post_ix: Self::IxSnapshot,
+            _ix_data: Self::IxData,
+        ) -> Result<(), FuzzingError> {
+
+            if let Some(escrow_pre) = pre_ix.escrow {
+                if pre_ix.receiver.key() != escrow_pre.receiver {
+                    if pre_ix.receiver.lamports() < post_ix.receiver.lamports() {
+                        return Err(FuzzingError::BalanceMismatch)
+                    }
+                }
+            }
+            Ok(())
+        }
+
+    }
+    #[doc = r" Use AccountsStorage<T> where T can be one of:"]
+    #[doc = r" Keypair, PdaStore, TokenStore, MintStore, ProgramStore"]
+    #[derive(Default)]
+    pub struct FuzzAccounts {
+        author: AccountsStorage<Keypair>,
+        escrow: AccountsStorage<PdaStore>,
+        receiver: AccountsStorage<Keypair>,
+    }
+}
---
+++ trident-tests/fuzz_tests/fuzz_0/test_fuzz.rs
@@ -0,0 +1,32 @@
+use fuzz_instructions::unauthorized_access_2_fuzz_instructions::Initialize;
+use unauthorized_access_2::entry;
+use unauthorized_access_2::ID as PROGRAM_ID;
+use fuzz_instructions::unauthorized_access_2_fuzz_instructions::FuzzInstruction;
+use trident_client::{convert_entry, fuzz_trident, fuzzing::*};
+mod accounts_snapshots;
+mod fuzz_instructions;
+
+const PROGRAM_NAME: &str = "unauthorized_access_2";
+
+struct MyFuzzData;
+
+impl FuzzDataBuilder<FuzzInstruction> for MyFuzzData {
+    fn pre_ixs(u: &mut arbitrary::Unstructured) ->
+        arbitrary::Result<Vec<FuzzInstruction>> {
+        let init_ix = FuzzInstruction::Initialize(Initialize::arbitrary(u)?);
+        Ok(vec![init_ix])
+    }
+
+}
+
+
+fn main() {
+    loop {
+        fuzz_trident!(fuzz_ix: FuzzInstruction, |fuzz_data: MyFuzzData| {
+            let mut client =
+                ProgramTestClientBlocking::new(PROGRAM_NAME, PROGRAM_ID, processor!(convert_entry!(entry)))
+                    .unwrap();
+            let _ = fuzz_data.run_with_runtime(PROGRAM_ID, &mut client);
+        });
+    }
+}
